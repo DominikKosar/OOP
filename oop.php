c74d07c4424bcc913f3b80eb4aee23be5398b64f
Dominik Košař <dom707@seznam.cz>
2023-04-12 19:01:08 +0200

    Objektově orientované programování-začátek

--- oop.php
+++ oop.php
@@ -1,5 +1,23 @@
 <?php 
-    echo "Ahoj";
+    
+    //třída - obecný vzor
+    class Clovek{
+        public function pozdrav(){
+            echo "Ahoj";
+        }
+    }
+
+    //tvorba objektu
+    $pavel = new Clovek();
+    $jana = new Clovek();
+    $dalibor = new Clovek();
+
+    //objekty umí pozdravit
+    $pavel ->pozdrav();
+    echo "<br>";
+    $jana ->pozdrav();
+    echo "<br>";
+    $dalibor ->pozdrav();
 
 
 ?>
