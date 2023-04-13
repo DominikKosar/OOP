944ac7560d6fdcfa79b9b50d0112a18b6a20ab92
Dominik Košař <dom707@seznam.cz>
2023-04-13 15:39:25 +0200

    Konstruktor v oop

--- oop.php
+++ oop.php
@@ -5,41 +5,30 @@
 
         public $jmeno;
         public $prijmeni;
+
+        public function __construct($jmeno, $prijmeni){
+            $this->jmeno = $jmeno;
+            $this->prijmeni = $prijmeni;
+        }
+
         public function pozdrav(){
             echo "Ahoj, já jsem ".$this->jmeno." ".$this->prijmeni;
         }
 
-        public function jduDoPrace(){
-            echo "Zrovna vyrážím do práce, ".$this->jmeno." a je ".date("H:i:s");
-        }
+        /*public function jduDoPrace(){
+            echo "Zrovna vyrážím do práce, ".$this->jmeno." a je ".date("H:i:s");   //hodiny, minuty, sekundy
+        }*/
     }
 
     //tvorba objektu
-    $pavel = new Clovek();
-    $pavel ->jmeno = "Pavel";
-    $pavel ->prijmeni = "Novák";
-    
-    $jana = new Clovek();
-    $jana ->jmeno = "Jana";
-    $jana ->prijmeni = "Novotná";
-
-    $dalibor = new Clovek();
-    $dalibor ->jmeno = "Dalibor";
-    $dalibor ->prijmeni = "Netkal";
-
+    $pavel = new Clovek("Pavel", "Novák");
+    $jana = new Clovek("Jana", "Nováková");
+    $dalibor = new Clovek("Dalibor", "Netkal");
     //objekty umí pozdravit
     $pavel ->pozdrav();
     echo "<br>";
     $jana ->pozdrav();
     echo "<br>";
     $dalibor ->pozdrav();
-    echo "<br>";
-
-    //objekty umí jít do práce
-    $pavel ->jduDoPrace();
-    echo "<br>";
-    $jana ->jduDoPrace();
-    echo "<br>";
-    $dalibor ->jduDoPrace();
 
 ?>
