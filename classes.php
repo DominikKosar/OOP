15e14ee52122f7b6e50b537f73f0eefe83532df2
Dominik Košař <dom707@seznam.cz>
2023-04-13 15:46:26 +0200

    Přejmenování souboru oop.php na classes.php

---
+++ classes.php
@@ -0,0 +1,45 @@
+<?php 
+    
+    //třída - obecný vzor
+    class Clovek{
+
+        public $jmeno;
+        public $prijmeni;
+
+        public function __construct($jmeno, $prijmeni){
+            $this->jmeno = $jmeno;
+            $this->prijmeni = $prijmeni;
+        }
+
+        public function pozdrav(){
+            echo "Ahoj, já jsem ".$this->jmeno." ".$this->prijmeni;
+        }
+
+        public function __toString(){
+            return $this->jmeno." ".$this->prijmeni;
+        }
+
+        /*public function jduDoPrace(){
+            echo "Zrovna vyrážím do práce, ".$this->jmeno." a je ".date("H:i:s");   //hodiny, minuty, sekundy
+        }*/
+    }
+
+    //tvorba objektu
+    $pavel = new Clovek("Pavel", "Novák");
+    $jana = new Clovek("Jana", "Nováková");
+    $dalibor = new Clovek("Dalibor", "Netkal");
+    //objekty umí pozdravit
+    $pavel ->pozdrav();
+    echo "<br>";
+    $jana ->pozdrav();
+    echo "<br>";
+    $dalibor ->pozdrav();
+    echo "<br>";
+    echo $pavel;
+    echo "<br>";
+    echo $jana;
+    echo "<br>";
+    echo $dalibor;
+    echo "<br>";
+
+?>
